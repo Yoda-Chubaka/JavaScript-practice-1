27853060c1a5862cdf9c1867fd6805d1af657240
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-04-18 11:00:32 +0300

    Update

---
+++ js/practice-1.js
@@ -0,0 +1,13 @@
+// Task #1
+// const productName = "Droid";
+// const pricePerItem = 2000;
+// console.log(productName);
+// console.log(pricePerItem);
+
+// Task #2
+let productName = "Droid";
+let pricePerItem = 2000;
+productName = "Repair droid";
+pricePerItem = 3500;
+console.log(productName);
+console.log(pricePerItem);
