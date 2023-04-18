5bf945a68cc15faa72c90399d664be3b98f7a5cb
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-04-18 17:14:22 +0300

    Update

--- js/practice-1.js
+++ js/practice-1.js
@@ -101,3 +101,129 @@
 // console.log(makeOrderMessage(4, 300, 100));
 // console.log(makeOrderMessage(10, 70, 200));
 
+// Task #13
+// function isAdult(age) {
+//   const passed = age>=18;
+//   return passed;
+// }
+// console.log(isAdult(20));
+// console.log(isAdult(14));
+// console.log(isAdult(8));
+// console.log(isAdult(37));
+
+// Task #14
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   const isMatch = password === SAVED_PASSWORD;
+//   return isMatch;
+// }
+// console.log(isValidPassword('mangodab3st'));
+// console.log(isValidPassword('kiwirul3z'));
+// console.log(isValidPassword('jqueryismyjam'));
+
+// Task #15
+// function checkAge(age) {
+//   let message;
+//   if (age >= 18) {
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+//   return message;
+// }
+// console.log(checkAge(20));
+// console.log(checkAge(14));
+
+// Task #16
+// function checkStorage(available, ordered) {
+//   let message;
+// if (ordered >= available) {
+//   message = "Not enough goods in stock!"
+// } else {
+//   message = "Order is processed, our manager will contact you."
+// }
+//   return message;
+// }
+// console.log(checkStorage(200, 50));
+// console.log(checkStorage(200, 250));
+
+// Task #17
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// Task #18
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+// const totalPrice = pricePerDroid * orderedQuantity;
+// if (totalPrice > customerCredits) {
+//   message = "Insufficient funds!";
+// } else {
+//   customerCredits -= totalPrice;
+//   message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
+// }
+//   return message;
+// }
+// console.log(makeTransaction(3000, 5, 23000));
+// console.log(makeTransaction(1000, 3, 15000));
+// console.log(makeTransaction(5000, 10, 8000));
+// console.log(makeTransaction(2000, 8, 10000));
+// console.log(makeTransaction(500, 10, 5000));
+
+// Task #19
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = 'jqueryismyjam';
+//   let message;
+//   if (password === null) {
+//     message =  'Canceled by user!';
+//   } else if (password === ADMIN_PASSWORD) {
+//     message = 'Welcome!';
+//   } else {
+//     message = 'Access denied, wrong password!';
+//   }
+//   return message;
+// }
+// console.log(checkPassword('mangohackzor'));
+// console.log(checkPassword(null));
+// console.log(checkPassword('polyhax'));
+// console.log(checkPassword('jqueryismyjam'));
+
+// Task #20
+// function checkStorage(available, ordered) {
+//   let message;
+// if (ordered === 0) {
+//   message = "There are no products in the order!";
+// } else if (ordered >available) {
+//   message = "Your order is too large, there are not enough items in stock!";
+// } else {
+//   message = "The order is accepted, our manager will contact you";
+// }
+//   return message;
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+
+// Task #21
+// function isNumberInRange(start, end, number) {
+//   const isInRange = number >= start && number <= end;
+//   return isInRange;
+// }
+// console.log(isNumberInRange(10, 30, 17));
+// console.log(isNumberInRange(10, 30, 5));
+// console.log(isNumberInRange(20, 50, 24));
+// console.log(isNumberInRange(20, 50, 76));
+
+// Task #22
