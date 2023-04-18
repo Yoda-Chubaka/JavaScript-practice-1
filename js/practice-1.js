8459d747ba8868e87c787076300cf96b5afb21df
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-04-18 14:33:36 +0300

    Update

--- js/practice-1.js
+++ js/practice-1.js
@@ -5,9 +5,50 @@
 // console.log(pricePerItem);
 
 // Task #2
-let productName = "Droid";
-let pricePerItem = 2000;
-productName = "Repair droid";
-pricePerItem = 3500;
-console.log(productName);
-console.log(pricePerItem);
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// productName = "Repair droid";
+// pricePerItem = 3500;
+// console.log(productName);
+// console.log(pricePerItem);
+
+// Task #3
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+// Task #4
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// Task #5
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// Task #6
+// Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid*orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// Task #7
+// function sayHi() {
+//   console.log("Hello, this is my first function!")
+// }
+// sayHi();
+
+// Task #8
