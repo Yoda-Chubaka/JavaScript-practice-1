7cda69446dbcc80b3a9babd547b319e4ac3b3c42
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-04-18 15:51:16 +0300

    Update

--- js/practice-1.js
+++ js/practice-1.js
@@ -79,3 +79,25 @@
 // console.log(makeMessage('Engine', 4070));
 
 // Task #11
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   const totalPrice = orderedQuantity*pricePerItem;
+//   return totalPrice;
+// };
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+// console.log(calculateTotalPrice(1, 3500));
+// console.log(calculateTotalPrice(12, 70));
+// console.log(calculateTotalPrice(5, 13));
+// console.log(calculateTotalPrice(7, 14));
+
+// Task #12
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   const totalPrice = orderedQuantity*pricePerDroid + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   return message;
+// }
+// console.log(makeOrderMessage(2, 100, 50));
+// console.log(makeOrderMessage(4, 300, 100));
+// console.log(makeOrderMessage(10, 70, 200));
+
